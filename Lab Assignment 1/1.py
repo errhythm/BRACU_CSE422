e8fdaa50ad05f2660c609063c5206935a4e90190
Ehsanur Rahman Rhythm <ehsanur.rahman.rhythm@g.bracu.ac.bd>
2022-10-26 23:24:23 +0600

    Completed, need some changes though

---
+++ Lab Assignment 1/1.py
@@ -0,0 +1,79 @@
+input_file = open("input.txt", "r")
+
+heuristic = {}
+city = {}
+
+for line in input_file:
+    line = line.split()
+    heuristic[line[0]] = int(line[1])
+    destinations = []
+    for i in range(2, len(line), 2):
+        destinations.append((line[i], int(line[i + 1])))
+    city[line[0]] = destinations
+
+
+def a_star_algorithm(start_node, stop_node):
+    open_list = set([start_node])
+    closed_list = set([])
+
+    g = {}
+
+    g[start_node] = 0
+
+    parents = {}
+    parents[start_node] = start_node
+
+    while len(open_list) > 0:
+        n = None
+
+        for v in open_list:
+            if n is None or g[v] + heuristic[v] < g[n] + heuristic[n]:
+                n = v
+
+        if n == None:
+            print('Path does not exist!')
+            return None
+
+        if n == stop_node:
+            result_path = []
+
+            while parents[n] != n:
+                result_path.append(n)
+                n = parents[n]
+
+            result_path.append(start_node)
+
+            result_path.reverse()
+
+            # print like Path: Arad -> Sibiu -> Rimnicu -> Pitesti -> Bucharest
+            print('Path: {}'.format(' -> '.join(result_path)))
+            # get the total cost of the path Total distance: 418 km
+            print('Total distance: {} km'.format(g[stop_node]))
+            return result_path
+
+        for (m, weight) in city[n]:
+            if m not in open_list and m not in closed_list:
+                open_list.add(m)
+                parents[m] = n
+                g[m] = g[n] + weight
+            else:
+                if g[m] > g[n] + weight:
+                    g[m] = g[n] + weight
+                    parents[m] = n
+
+                    if m in closed_list:
+                        closed_list.remove(m)
+                        open_list.add(m)
+
+        open_list.remove(n)
+        closed_list.add(n)
+
+    print('NO PATH FOUND')
+    return None
+
+
+# start_node = input("Enter the start node: ")
+# destination_node = input("Enter the destination node: ")
+start_node = "Arad"
+destination_node = "Bucharest"
+a_star_algorithm(start_node, destination_node)
